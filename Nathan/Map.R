9e093daa7b0d487e15e8df80c8a70719dde7684d
nmset0 <nmset0@colostate.edu>
2024-02-21 15:49:46 -0700

    Starting on map

---
+++ Nathan/Map.R
@@ -0,0 +1,14 @@
+library(leaflet)
+library(readr)
+library(dplyr)
+
+gunViolence = read.csv("C:/Users/natha/OneDrive/Desktop/School/gun-violence-data_01-2013_03-2018.csv")
+gunViolence = na.omit(gunViolence)
+
+map = leaflet() %>% 
+  addTiles() %>% 
+  setView(lng = -98.5795, lat = 39.8283, zoom = 3.5) %>%
+  addCircleMarkers(, lng = gunViolence$longitude, lat = gunViolence$latitude, radius = 1)
+map
+
+
