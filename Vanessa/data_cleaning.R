43a1d508073d6f41e0d0955fc43a9e8affc34c5c
Vanessa Kokoszka <vanessakokoszka@Vanessas-MacBook-Pro-2.local>
2024-02-21 15:08:41 -0700

    small edit

--- Vanessa/data_cleaning.R
+++ Vanessa/data_cleaning.R
@@ -1,7 +1,5 @@
 library(tidyverse)
-library(dplyr)
 #library(latlong2)
-library(stringr)
 
 #reading in original file
 orig_df <- read_csv("~/Desktop/Statistics/STAT 472 - Project/gun_violence_data.csv")
@@ -23,6 +21,24 @@ orig_df |>
          -state_house_district, 
          -state_senate_district) -> lean_df
 
+#manually adding the las vegas shooting on oct 1, 2017
+las_vegas <- tibble(incident_id = "0", 
+               date = date("2017-10-01"), 
+               state = "Nevada", 
+               city_or_county = "Clark (county)", 
+               n_killed = 59, 
+               n_injured = 489, 
+               gun_type = NA, 
+               incident_characteristics = NA, 
+               latitude = 36.095, 
+               location_description = "Hotel", 
+               longitude = -115.171667, 
+               n_guns_involved = 24, 
+               participant_age = 64, 
+               participant_age_group = NA, 
+               participant_gender = "0::Male")
+lean_df <- rbind(lean_df, las_vegas)
+
 #adding more columns for date information
 date_decomp <- tibble(incident_year = year(lean_df$date),
                       incident_month = month(lean_df$date, label = TRUE),
@@ -40,10 +56,13 @@ lat_long_df <- tibble(longitude = as.numeric(lean_df$longitude),
 
 #taking the age column, parsing through the text, and turning it into mean age
 ages_to_mean_age <- function(a_str){
+  if(is.na(a_str)){
+    return(NA)
+  }
   a_str |>
     str_extract_all("(?<=::)\\d+") |> 
     unlist() |> 
-    as.numeric() |> 
+    as.numeric( ) |> 
     mean() -> mean_age
   mean_age <- format(round(mean_age, 2), nsmall = 2)
   return(mean_age)
