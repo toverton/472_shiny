08d0b06082f633fff0982af433f1ff6b55110f8e
nmset0 <nmset0@colostate.edu>
2024-02-26 15:07:49 -0700

    deleted columns in script

--- Nathan/Map.R
+++ Nathan/Map.R
@@ -4,6 +4,16 @@ library(dplyr)
 
 gunViolence = read.csv("C:/Users/natha/OneDrive/Desktop/School/gun-violence-data_01-2013_03-2018.csv")
 gunViolence = na.omit(gunViolence)
+gunViolence = subset(gunViolence, select = -c(incident_id, notes, 
+                                              incident_url_fields_missing, 
+                                              location_description, 
+                                              participant_relationship,
+                                              sources,
+                                              participant_type,
+                                              participant_status, 
+                                              incident_url, 
+                                              source_url, gun_stolen, gun_type, 
+                                              incident_characteristics, n_guns_involved))
 
 map = leaflet() %>% 
   addTiles() %>% 
