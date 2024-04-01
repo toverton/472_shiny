fde247c18cf85c84e673c79401ad8738b7881a04
Vanessa Kokoszka <vanessakokoszka@Vanessas-MacBook-Pro-2.local>
2024-04-01 15:12:08 -0600

    testing for trend

--- Vanessa/death_rate.R
+++ Vanessa/death_rate.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(readxl)
 library(reshape2)
 library(ggplot2)
+library(funtimes)
 
 final_df |> 
   filter(year %in% c(2014, 2015, 2016, 2017)) |> 
@@ -27,7 +28,13 @@ f_df_14_17 |>
   merge(us_pop_14_17, by = "year") |> 
   mutate(deaths_per_mill = (total_killed / us_population)*1000000) -> death_rate_df
 
-rm(yearly_pop, f_df_14_17)
+rm(yearly_pop, f_df_14_17) #keeping environment clean
+
+death_rate_df |> 
+  select(date, deaths_per_mill) -> death_rate_ts
+death_rate_ts <- ts(death_rate_ts$deaths_per_mill, death_rate_ts$date)
+
+notrend_test(death_rate_ts)
 
 #write.csv(death_rate_df, "~/Desktop/death_rate_df.csv", row.names = F)
 
